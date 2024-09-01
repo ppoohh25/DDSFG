5b5dee1054e1cd8ca9fe8ec1fbd079a167eace8e
Sadayu Dachdachasunan <93990764+ppoohh25@users.noreply.github.com>
2024-09-01 18:55:34 +0700

    Update Coef.py
    fix bug that cause overflow

--- Coef/Coef.py
+++ Coef/Coef.py
@@ -1,6 +1,6 @@
 import math
 
-f = open("coefficient.mi","w")
+f = open("coefficient3.mi","w")
 
 f.write("#File_format=Hex\n")
 f.write("#Address_depth=2048\n")
@@ -9,13 +9,13 @@ f.write("#Data_width=48\n")
 n = 100
 
 for i in range(0,2048):
-    alpha = math.sin(2*math.pi*n/24000) * 0.95
-    alpha = round(alpha *536870912, 0)
+    alpha1 = math.sin(2*math.pi*n/24000) * 0.95
+    alpha = round(alpha1 *536870912, 0)
     alpha = int(alpha) >> 4
 
-    y1 = math.cos(2*math.pi*n/24000)
-    y1 = round (y1 * 1073741824, 0)
-    y1 = (int(y1) & 0x01FFFFFE) >> 1
+    y11 = math.cos(2*math.pi*n/24000) 
+    y12 = round (y11 * 1073741824, 0)
+    y1 = (int(y12) & 0x03FFFFFC) >> 2
 
     if (n <= 1000):
         f.write("%06x%06x \n" % (alpha, y1))
@@ -23,3 +23,5 @@ for i in range(0,2048):
     else :
         f.write("%06x%06x \n" % (0, 0))
         n = n + 0.5
+
+    x = x+1    
