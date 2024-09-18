c285737613f3fe7e252e88d0e9db35e463c4aa92
Sadayu Dachdachasunan <93990764+ppoohh25@users.noreply.github.com>
2024-09-18 19:33:16 +0700

    update Interpolator

--- Interpolator.v
+++ Interpolator.v
@@ -14,7 +14,7 @@ module Interpolator (
   reg [63:0] delta; 
   reg [11:0] InterpOut1;
 
-  // Compute delta combinationally
+//   Compute delta combinationally
   always @(*) begin
     case(Mode) 
       0 : const = 32'd1;
@@ -27,7 +27,7 @@ module Interpolator (
   end
 
   always @(*) begin
-    delta = $signed(Out1 - Out2) * $signed(const);
+    delta <= ($signed(Out1) - $signed(Out2)) * $signed(const);
   end
 
   // Resetting and updating Enable_d
@@ -54,11 +54,6 @@ module Interpolator (
   always @(*) begin
     InterpOut1 = out[29:18]; // 29:18
   end 
-  /*
-  ---------------------------- Error at address 1700-1800 -----------------------------
-    progress --> if (address > 1699) InterpOut1 = out[31:20]; fix some issue but not all.
-  -------------------------------------------------------------------------------------
-  */
 
   // Updating InterpOut
   always @(posedge Fg_clk or negedge Resetn) begin
@@ -70,58 +65,3 @@ module Interpolator (
   end
 
 endmodule
-
-// module Interpolator(Fg_clk, Resetn, Out1, Out2, Mode, Enable, InterpOut);
-//     input Fg_clk;
-//     input Resetn;
-//     input [31:0] Out1;
-//     input [31:0] Out2;
-//     input [2:0] Mode;
-//     input Enable;
-
-//     output wire [11:0] InterpOut; //12 bit unsigned
-
-//     reg Enable_delay;
-//     reg [31:0] delta_y;
-//     reg [31:0] interpOut_buffer;
-//     reg [31:0] delta_y_buffer;
-//     reg [11:0] interpOut; // 12 bit signed
-
-//     assign InterpOut =  {~interpOut[11], interpOut[10:0]}; //offset binary, unsigned
-  
-
-//     always @(*)begin //combination
-//         delta_y_buffer <=  $signed(Out1-Out2)/$signed(10**Mode);
-//     end
-
-//     always@(posedge Fg_clk or negedge Resetn)begin
-//         if(~Resetn) Enable_delay <= 0;
-//         else        Enable_delay <= Enable;
-//     end
-
-//     always@(posedge Fg_clk or negedge Resetn)begin
-//         if(~Resetn) delta_y <= 0;
-//         else        delta_y <= delta_y_buffer;//delta_y <= (Out2-Out1)/(10**Mode);//! /,** ไม่น่าทำงานได้ ต้องทำใหม่
-            
-
-//     end
-
-//     always @(posedge Fg_clk or negedge Resetn)begin
-//         if(~Resetn) begin
-//             interpOut_buffer <= 0; //! Out1, Out2 ต้องresetไหม
-//             interpOut <= 0;
-
-//         end
-//         else if (Enable_delay) begin
-//             interpOut_buffer <= Out2;
-//             interpOut <= interpOut_buffer[29:18];
-//         end
-//         else begin
-//             interpOut_buffer <= interpOut_buffer + delta_y;
-//             interpOut <= interpOut_buffer[29:18];//{~interpOut_buffer[31], interpOut_buffer[30:0]};
-//         end
-
-//     end
-
-
-// endmodule
